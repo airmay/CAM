6fa405a29fcfe7f7b6710fa164f49c28bad38905
Andrey Mikov <airmay@gmail.com>
2019-03-23 13:19:15 +0500

    add acad

--- AcadGateway.cs
+++ AcadGateway.cs
@@ -1,20 +1,80 @@
-﻿using CAM.Domain;
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Windows;
+using CAM.Domain;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CAM
 {
     /// <summary>
     /// Класс осуществляющий взаимодействие с автокадом
     /// </summary>
-    //public class AcadGateway : IAcadGateway
-    //{
-    //    public Curve[] GetSelectedCurves()
-    //    {
-    //        return new Curve[] { new Line(new Point3d(0, 0, 0), new Point3d(1, 0, 0)), new Arc(new Point3d(0, 0, 0), 1, 0, 1) };
-    //    }
-    //}
+    public class AcadGateway : IAcadGateway
+    {
+        public void CreateEntities(List<Curve> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateEntities(IEnumerable<Curve> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEntities(IEnumerable<Curve> idList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Curve[] GetSelectedCurves()
+        {
+            return new Curve[] { new Line(new Point3d(0, 0, 0), new Point3d(1, 0, 0)), new Arc(new Point3d(0, 0, 0), 1, 0, 1) };
+        }
+
+        public Curve[] GetSelectedEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectCurve(Curve curve)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SelectEntities(List<ObjectId> idList)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public static void AddPaletteSet(PaletteSet paletteSet, string name, Control control)
+        {
+            if (paletteSet == null)
+                paletteSet = new PaletteSet("Технология")
+                {
+                    Style = PaletteSetStyles.NameEditable | PaletteSetStyles.ShowPropertiesMenu | PaletteSetStyles.ShowAutoHideButton | PaletteSetStyles.ShowCloseButton,
+                    MinimumSize = new Size(300, 200),
+                    KeepFocus = true,
+                    Visible = true
+                };
+            paletteSet.Add(name, control);
+        }
+
+        internal PaletteSet CreatePaletteSet()
+        {
+            return new PaletteSet("Технология")
+            {
+                Style = PaletteSetStyles.NameEditable | PaletteSetStyles.ShowPropertiesMenu | PaletteSetStyles.ShowAutoHideButton | PaletteSetStyles.ShowCloseButton,
+                MinimumSize = new Size(300, 200),
+                KeepFocus = true,
+                Visible = true
+            };
+        }
+    }
 }
---
+++ Bootstrapper.cs
@@ -0,0 +1,78 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+using CAM.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAM
+{
+    public class Bootstrapper : IExtensionApplication
+    {
+        public void Initialize()
+        {
+            var acad = new AcadGateway();
+            PaletteSet paletteSet = acad.CreatePaletteSet();
+            //AutocadUtils.WriteMessage("Инициализация плагина ProcessingProgram. Версия с режимом обработки."); // + DateTime.Today.ToShortDateString()); TODO Assemlly.DateTime()
+
+            paletteSet.Add("Объекты", new TechProcessView());
+            paletteSet.Add("Программа", new ProgramView());
+
+            //ProcessingForm.CurrentChanged += (sender, args) => AutocadUtils.SelectObjects(args.Data.ObjectId, args.Data.ToolObjectId);
+            //ProcessingForm.DeleteProcessing += (sender, args) => DeleteProcessing();
+            //ProcessingForm.Calculate += (sender, args) => Calculate();
+            //AutocadUtils.AddPaletteSet("Обработка", ProcessingForm);
+
+            //AutocadUtils.AddPaletteSet("Программа", ProgramForm);
+            //AutocadUtils.AddPaletteSet("Настройки", SettingForm);
+
+            //AutocadUtils.Selected += (sender, args) => ProcessingForm.SelectObjects(args.Data);
+            //AutocadUtils.Focused += (sender, args) => ProcessingForm.SetFocus(args.Data);
+            //AutocadUtils.Focused += (sender, args) => ObjectForm.SetFocus(args.Data);
+
+            //CalcUtils.Init(ProcessObjects, ProcessCurves);
+            //ProcessObjectFactory.Init(ProcessObjects, ProcessCurves);
+
+            //var machine = new Machine();
+            //machine.ChangeActionsCount += (sender, args) => ObjectForm.ShowProgress(String.Format("Генерация обработки... {0} строк", args.Data));
+            //ActionGenerator.SetMachine(machine);
+
+            //AutocadUtils.CreateTest();
+            //RunTest();
+        }
+
+        public void Terminate()
+        {
+            //SettingForm.RefreshSettings();
+            //ProcessingParams.SaveDefault();
+            //Settings.Save();
+        }
+
+        [CommandMethod("show")]
+        public void ShowPaletteSet()
+        {
+            //AutocadUtils.ShowPaletteSet();
+        }
+
+        [Conditional("DEBUG")]
+        private void RunTest()
+        {
+            //var selectedObjects = AutocadUtils.GetAllCurves();
+            //if (selectedObjects == null || !Tools.Any())
+            //    return;
+            //ProcessObjectFactory.Create(selectedObjects.FindAll(p => p.GetLength() > 100), Tools.Where(p => p.No == 2));
+            ///*
+            //SectionCurves.AddRange(selectedObjects.FindAll(p => p.GetLength() < 100).Cast<Curve>().ToList());
+            //var points = SectionCurves.Select(p => p.StartPoint.Y).Concat(SectionCurves.Select(p => p.EndPoint.Y));
+            //Settings.GetInstance().HeightMax = points.Max();
+            //Settings.GetInstance().HeightMin = points.Min();
+            //SettingForm.RefreshForm();
+            // * */
+            //Calculate();
+            //ObjectForm.RefreshList();
+        }
+    }
+}
