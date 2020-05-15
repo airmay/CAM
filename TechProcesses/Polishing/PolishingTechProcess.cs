e3e77e7abc189403f3ee93ee97428676c0fbe300
Andrey Mikov <airmay@gmail.com>
2020-05-15 22:21:58 +0500

    Полировка пила

--- TechProcesses/Polishing/PolishingTechProcess.cs
+++ TechProcesses/Polishing/PolishingTechProcess.cs
@@ -2,6 +2,7 @@
 using Autodesk.AutoCAD.Geometry;
 using Dreambuild.AutoCAD;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CAM.TechProcesses.Polishing
@@ -12,17 +13,17 @@ namespace CAM.TechProcesses.Polishing
     {
         public int Feed { get; set; }
 
-        public double Angle { get; set; }
+        public double Angle1 { get; set; }
 
-        public double Step1 { get; set; }
+        public double Angle2 { get; set; }
 
-        public double Step2 { get; set; }
+        public int StepMin { get; set; }
 
-        public double Amplitude { get; set; }
+        public int StepMax { get; set; }
 
-        public bool IsRandomStepCount { get; set; }
+        public int AmplitudeMin { get; set; }
 
-        public bool IsRandomStepParams { get; set; }
+        public int AmplitudeMax { get; set; }
 
         public PolishingTechProcess(string caption) : base(caption)
         {
@@ -36,59 +37,116 @@ namespace CAM.TechProcesses.Polishing
             else
                 generator.SetTool(1, Frequency, angleA: 0, hasTool: false);
 
+            var curves = ProcessingArea.Select(p => p.ObjectId).QOpenForRead<Curve>().ToList();
             var bounds = ProcessingArea.Select(p => p.ObjectId).GetExtents();
-            var ray = new Ray
-            {
-                BasePoint = new Point3d(bounds.MinPoint.X, 0, 0),
-                UnitDir = Vector3d.YAxis
-            };
-            var uppDir = true;
             var random = new Random();
-
-            while (true)
-            {
-                var points = ray.Intersect(ProcessingArea.Select(p => p.ObjectId).QOpenForRead<Curve>().ToList(), Intersect.ExtendThis);
-                if (points.Count == 0)
-                    break;
-                if (points.Count > 1)
+            var mainDir = Vector3d.XAxis.RotateBy(Angle1.ToRad(), Vector3d.ZAxis);
+            var side = 1;
+            var basePoint = bounds.MinPoint + (bounds.GetCenter() - bounds.MinPoint).GetNormal() * 100;
+            using (var ray = new Ray())
+                while (true)
                 {
-                    points = (uppDir ? points.OrderBy(p => p.Y) : points.OrderByDescending(p => p.Y)).ToList();
-                    Cutting(points.First(), points.Last());
-                }
-                ray.BasePoint += Vector3d.XAxis * Step1;
-            }
+                    ray.BasePoint = basePoint;
+                    ray.UnitDir = mainDir.RotateBy(Angle2.ToRad() * side, Vector3d.ZAxis);
+                    var points = ray.Intersect(curves).FindAll(p => p.DistanceTo(ray.BasePoint) > 10);
+                    if (!points.Any())
+                        break;
 
-            void Cutting(Point3d point1, Point3d point2)
+                    basePoint = points.First() + ray.UnitDir.Negate();
+                    Cutting(ray.BasePoint, basePoint, ray.UnitDir, ray.UnitDir.GetPerpendicularVector() * side);
+                    side *= -1;
+                }
+
+            void Cutting(Point3d point1, Point3d point2, Vector3d dir, Vector3d pv)
             {
                 if (generator.IsUpperTool)
                     generator.Move(point1.X, point1.Y);
                 generator.GCommand(CommandNames.Cutting, 1, point: point1);
 
+                var point0 = point1;
+                var line = new Line();
                 var length = point1.DistanceTo(point2);
-                var rnd = IsRandomStepCount ? (random.NextDouble() / 2 + 0.75) : 1;
-                var countCycles = (int)Math.Round(length * rnd / Step2 / 2) * 2;
-                var coodrs = Enumerable.Range(1, countCycles).Select(p => (double)p);
-                if (IsRandomStepParams)
-                    coodrs = coodrs.Select(p => p + (p != countCycles ? (random.NextDouble() / 2 - 0.25) : 0));
-                var coordArray = coodrs.ToArray();
-                double l = 0;
+                var l = 0;
                 double a = 0;
                 var count = 10;
                 var stepA = Math.PI / count;
-                for (int i = 0; i < coordArray.Length; i++)
+                var stepL = 0;
+                var amp = 0;
+                var isInner = true;
+
+                while (l < length)
                 {
-                    var coord0 = i > 0 ? coordArray[i - 1] : 0;
-                    var dl = coordArray[i] - coord0;
-                    var stepL = dl * length / countCycles / count;
-                    
-                    foreach (var j in Enumerable.Range(1, count))
+                    if (Math.Abs(Math.Sin(a)) < Consts.Epsilon)
                     {
-                        l += stepL;
-                        a += stepA;
-                        var point = point1 + new Vector3d(Math.Sin(a) * Amplitude * dl, uppDir ? l : -l, 0);
-                        generator.GCommand(CommandNames.Cutting, 1, point: point, feed: Feed);
+                        stepL = random.Next(StepMin, StepMax) / count;
+                        amp = random.Next(AmplitudeMin, AmplitudeMax);
                     }
+                    l += stepL;
+                    a += stepA;
+                    var point = point1 + dir * l + pv * Math.Sin(a) * amp;
+                    line.StartPoint = point0;
+                    line.EndPoint = point;
+                    if (line.Intersect(curves).Any())
+                        isInner = !isInner;
+                    if (isInner)
+                        generator.GCommand(CommandNames.Cutting, 1, point: point, feed: Feed);
+                    point0 = point;
                 }
+                line.Dispose();
+            }
+
+                //var ray = new Ray
+                //{
+                //    BasePoint = new Point3d(bounds.MinPoint.X, 0, 0),
+                //    UnitDir = Vector3d.YAxis
+                //};
+                //var uppDir = true;
+                //var random = new Random();
+
+                //while (true)
+                //{
+                //    var points = ray.Intersect(ProcessingArea.Select(p => p.ObjectId).QOpenForRead<Curve>().ToList(), Intersect.ExtendThis);
+                //    if (points.Count == 0)
+                //        break;
+                //    if (points.Count > 1)
+                //    {
+                //        points = (uppDir ? points.OrderBy(p => p.Y) : points.OrderByDescending(p => p.Y)).ToList();
+                //        Cutting(points.First(), points.Last());
+                //    }
+                //    ray.BasePoint += Vector3d.XAxis * Step1;
+                //}
+
+                //void Cutting(Point3d point1, Point3d point2)
+                //{
+                //    if (generator.IsUpperTool)
+                //        generator.Move(point1.X, point1.Y);
+                //    generator.GCommand(CommandNames.Cutting, 1, point: point1);
+
+                //    var length = point1.DistanceTo(point2);
+                //    var rnd = IsRandomStepCount ? (random.NextDouble() / 2 + 0.75) : 1;
+                //    var countCycles = (int)Math.Round(length * rnd / Step2 / 2) * 2;
+                //    var coodrs = Enumerable.Range(1, countCycles).Select(p => (double)p);
+                //    if (IsRandomStepParams)
+                //        coodrs = coodrs.Select(p => p + (p != countCycles ? (random.NextDouble() / 2 - 0.25) : 0));
+                //    var coordArray = coodrs.ToArray();
+                //    double l = 0;
+                //    double a = 0;
+                //    var count = 10;
+                //    var stepA = Math.PI / count;
+                //    for (int i = 0; i < coordArray.Length; i++)
+                //    {
+                //        var coord0 = i > 0 ? coordArray[i - 1] : 0;
+                //        var dl = coordArray[i] - coord0;
+                //        var stepL = dl * length / countCycles / count;
+
+                //        foreach (var j in Enumerable.Range(1, count))
+                //        {
+                //            l += stepL;
+                //            a += stepA;
+                //            var point = point1 + new Vector3d(Math.Sin(a) * Amplitude * dl, uppDir ? l : -l, 0);
+                //            generator.GCommand(CommandNames.Cutting, 1, point: point, feed: Feed);
+                //        }
+                //    }
 
                 //while(true)
                 //{
@@ -115,8 +173,8 @@ namespace CAM.TechProcesses.Polishing
                 //    l += stepL;
                 //    a += stepA;
                 //}
-                uppDir = !uppDir;
+                //    uppDir = !uppDir;
+                //}
             }
-        }
     }
 }
--- TechProcesses/Polishing/PolishingTechProcessView.Designer.cs
+++ TechProcesses/Polishing/PolishingTechProcessView.Designer.cs
@@ -52,8 +52,10 @@
             this.label3 = new System.Windows.Forms.Label();
             this.tbStep2 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.polishingTechProcessBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
@@ -205,7 +207,7 @@
             // 
             this.tbAngle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbAngle.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Angle", true));
+            this.tbAngle.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Angle1", true));
             this.tbAngle.Location = new System.Drawing.Point(102, 175);
             this.tbAngle.Name = "tbAngle";
             this.tbAngle.Size = new System.Drawing.Size(152, 20);
@@ -216,15 +218,15 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(3, 178);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.Size = new System.Drawing.Size(38, 13);
             this.label1.TabIndex = 105;
-            this.label1.Text = "Угол";
+            this.label1.Text = "Угол1";
             // 
             // tbStep1
             // 
             this.tbStep1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbStep1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Step1", true));
+            this.tbStep1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Angle2", true));
             this.tbStep1.Location = new System.Drawing.Point(102, 198);
             this.tbStep1.Name = "tbStep1";
             this.tbStep1.Size = new System.Drawing.Size(152, 20);
@@ -235,16 +237,16 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(3, 201);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 107;
-            this.label2.Text = "Шаг1";
+            this.label2.Text = "Угол2";
             // 
             // tbAmplitude
             // 
             this.tbAmplitude.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbAmplitude.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Amplitude", true));
-            this.tbAmplitude.Location = new System.Drawing.Point(102, 221);
+            this.tbAmplitude.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "AmplitudeMin", true));
+            this.tbAmplitude.Location = new System.Drawing.Point(102, 234);
             this.tbAmplitude.Name = "tbAmplitude";
             this.tbAmplitude.Size = new System.Drawing.Size(152, 20);
             this.tbAmplitude.TabIndex = 90;
@@ -252,18 +254,18 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(3, 224);
+            this.label3.Location = new System.Drawing.Point(3, 237);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.Size = new System.Drawing.Size(88, 13);
             this.label3.TabIndex = 109;
-            this.label3.Text = "Амплитуда";
+            this.label3.Text = "Амплитуда мин.";
             // 
             // tbStep2
             // 
             this.tbStep2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbStep2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "Step2", true));
-            this.tbStep2.Location = new System.Drawing.Point(102, 244);
+            this.tbStep2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "AmplitudeMax", true));
+            this.tbStep2.Location = new System.Drawing.Point(102, 257);
             this.tbStep2.Name = "tbStep2";
             this.tbStep2.Size = new System.Drawing.Size(152, 20);
             this.tbStep2.TabIndex = 100;
@@ -271,40 +273,58 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(3, 247);
+            this.label4.Location = new System.Drawing.Point(3, 260);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.Size = new System.Drawing.Size(94, 13);
             this.label4.TabIndex = 111;
-            this.label4.Text = "Шаг2";
+            this.label4.Text = "Амплитуда макс.";
             // 
-            // checkBox1
+            // textBox1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.polishingTechProcessBindingSource, "IsRandomStepCount", true));
-            this.checkBox1.Location = new System.Drawing.Point(6, 273);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(165, 17);
-            this.checkBox1.TabIndex = 112;
-            this.checkBox1.Text = "Изменять количество волн";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "StepMax", true));
+            this.textBox1.Location = new System.Drawing.Point(102, 317);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 113;
             // 
-            // checkBox2
+            // label7
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.polishingTechProcessBindingSource, "IsRandomStepParams", true));
-            this.checkBox2.Location = new System.Drawing.Point(6, 296);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(164, 17);
-            this.checkBox2.TabIndex = 113;
-            this.checkBox2.Text = "Изменять параметры волн";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 115;
+            this.label7.Text = "Шаг макс.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.polishingTechProcessBindingSource, "StepMin", true));
+            this.textBox2.Location = new System.Drawing.Point(102, 294);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 112;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 297);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.TabIndex = 114;
+            this.label9.Text = "Шаг мин.";
             // 
             // PolishingTechProcessView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.tbStep2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.tbAmplitude);
@@ -360,7 +380,9 @@
         private System.Windows.Forms.TextBox tbStep2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.BindingSource polishingTechProcessBindingSource;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
     }
 }
