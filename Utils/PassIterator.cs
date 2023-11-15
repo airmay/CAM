3bab32dc5dbf7f29afbb4b4af11479d72d610aed
mikov <mikov@lanit.ru>
2023-11-15 08:17:29 +0500

    PassIterator

---
+++ Utils/PassIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.AutoCAD.Geometry;
+
+namespace CAM.Utils
+{
+    public static class PassIterator
+    {
+        public static IEnumerable<Pass> Create(Point3d point1, Point3d point2)
+        {
+            while (true)
+            {
+                yield return new Pass(point1, point2);
+                yield return new Pass(point2, point1);
+            }
+        }
+
+        public static IEnumerable<Pass> WithOffset(this IEnumerable<Pass> source, Vector3d vector)
+        {
+            return source.Select((p, i) => new Pass(p.Start.Add(vector * i), p.End.Add(vector * i)));
+        }
+
+        public static IEnumerable<Pass> Take(this IEnumerable<Pass> source, Vector3d vector, Vector3d limit)
+        {
+            var count = (int)Math.Ceiling(limit.Length / vector.Length) + 1;
+            return source.Select((p, i) =>
+                {
+                    var offset = i < count - 1 ? vector * i : limit;
+                    return new Pass(p.Start.Add(offset), p.End.Add(offset));
+                })
+                .Take(count);
+        }
+
+        public static IEnumerable<Pass> TakeZ(this IEnumerable<Pass> source, double dz, double zMax)
+        {
+            return source.Take(-Vector3d.ZAxis * dz, -Vector3d.ZAxis * zMax);
+        }
+
+    }
+}
