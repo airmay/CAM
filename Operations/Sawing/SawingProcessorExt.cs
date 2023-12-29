c63cb3c5e3f8d3eafe658dbc2b1c2e24cfc9d6ae
Миков Андрей <mikov@lanit.ru>
2023-12-29 21:33:19 +0500

    SawingProcessorExt CuttingArc

--- Operations/Sawing/SawingProcessorExt.cs
+++ Operations/Sawing/SawingProcessorExt.cs
@@ -2,6 +2,7 @@
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using Dreambuild.AutoCAD;
+using System.Security.Cryptography;
 
 namespace CAM.Operations.Sawing
 {
@@ -10,16 +11,15 @@ namespace CAM.Operations.Sawing
         public static void Cutting(this Processor processor, Curve curve, CurveTip tip, Side side, double depth,
             bool isExactlyBegin, bool isExactlyEnd, double indent)
         {
+            var toolpath = (Curve) curve.GetTransformedCopy(Matrix3d.Displacement(-Vector3d.ZAxis * depth));
             if (processor.IsUpperTool)
             {
-                var toolpath = CreateToolpath(baseCurve, passList[0].Item1);
                 var startPoint = toolpath.GetPoint(tip);
                 var angleC = BuilderUtils.CalcToolAngle(toolpath, startPoint, engineSide);
                 processor.Move(startPoint.X, startPoint.Y, angleC);
                 processor.Cycle();
 
             }
-            var toolpath = curve.GetTransformedCopy(Matrix3d.Displacement(-Vector3d.ZAxis * depth));
             switch (toolpath)
             {
                 case Line line:
@@ -54,9 +54,21 @@ namespace CAM.Operations.Sawing
                 if (isExactlyBegin) arc.StartAngle += indentAngle;
                 if (isExactlyEnd) arc.EndAngle -= indentAngle;
 
+                var angleC = BuilderUtils.CalcToolAngle(curve, point, engineSide);
+
                 var angleC = processor.AngleC + arc.TotalAngle.ToDeg() * (tip == curve.StartPoint ? -1 : 1);
                 var gCode = tip == arc.StartPoint ? 3 : 2;
                 processor.GCommand(CommandNames.Cutting, gCode, point: curve.NextPoint(tip), angleC: angleC, curve: curve, feed: cuttingFeed, center: arc?.Center.ToPoint2d());
+
+                var point = arc.GetPoint(tip);
+                if (processor.IsUpperTool)
+                {
+                    var angleC = BuilderUtils.CalcToolAngle(line.Angle, engineSide);
+                    processor.Move(point.ToPoint2d(), angleC);
+                    processor.Cycle();
+                }
+                processor.Penetration(point);
+                Cutting(arc, arc.NextPoint(point));
 
             }
 
@@ -68,6 +80,15 @@ namespace CAM.Operations.Sawing
                         polyline.GetPointAtDist(polyline.Length - indent).ToPoint2d());
 
             }
+        }
+
+        private static void Cutting(Arc arc, Point3d point)
+        {
+            if (arc != null && calcAngleC)
+                angleC += arc.TotalAngle.ToDeg() * (point == curve.StartPoint ? -1 : 1);
+            var gCode = curve is Line ? 1 : point == curve.StartPoint ? 3 : 2;
+            GCommand(CommandNames.Cutting, gCode, point: curve.NextPoint(point), angleC: angleC, curve: curve, feed: cuttingFeed, center: arc?.Center.ToPoint2d());
+
         }
     }
 }
