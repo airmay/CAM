a70e78e69904acbac56ac38dee153a3426374c8b
Миков Андрей <mikov@lanit.ru>
2025-10-14 14:10:31 +0500

    ProcessingWireSaw

--- MachineWireSaw/ProcessingWireSaw.cs
+++ MachineWireSaw/ProcessingWireSaw.cs
@@ -1,45 +1,44 @@
 ﻿using System;
 using CAM.MachineWireSaw;
 
-namespace CAM.CncWorkCenter
+namespace CAM.CncWorkCenter;
+
+[Serializable]
+public class ProcessingWireSaw : ProcessingBase<ProcessingWireSaw, ProcessorWireSaw>
 {
-    [Serializable]
-    public class ProcessingWireSaw : ProcessingBase<ProcessingWireSaw, ProcessorWireSaw>
+    public double ToolThickness
     {
-        public double ToolThickness
-        {
-            get => Tool.Thickness.GetValueOrDefault();
-            set => Tool.Thickness = value;
-        }
-        public int CuttingFeed { get; set; } = 10;
-        public int S { get; set; } = 100;
-        public double Approach { get; set; } = 50;
-        public double Departure { get; set; } = 50;
-        public double Delta { get; set; } = 5;
+        get => Tool.Thickness.GetValueOrDefault();
+        set => Tool.Thickness = value;
+    }
+    public int CuttingFeed { get; set; } = 10;
+    public int S { get; set; } = 100;
+    public double Approach { get; set; } = 50;
+    public double Departure { get; set; } = 50;
+    public double Delta { get; set; } = 5;
 
-        public static void ConfigureParamsView(ParamsControl view)
-        {
-            view.AddOrigin();
-            view.AddTextBox(nameof(CuttingFeed));
-            view.AddTextBox(nameof(S), "Угловая скорость", hint: "Z безопасности отсчитывается от верха выбранных объектов техпроцесса");
+    public static void ConfigureParamsView(ParamsControl view)
+    {
+        view.AddOrigin();
+        view.AddTextBox(nameof(CuttingFeed), required: true);
+        view.AddTextBox(nameof(S), "Угловая скорость", required: true);
             
-            view.AddIndent();
-            view.AddTextBox(nameof(ToolThickness), "Толщина троса", required: true);
-            view.AddTextBox(nameof(Approach), "Заезд");
-            view.AddTextBox(nameof(Departure), "Выезд");
+        view.AddIndent();
+        view.AddTextBox(nameof(ToolThickness), "Толщина троса", required: true);
+        view.AddTextBox(nameof(Approach), "Заезд");
+        view.AddTextBox(nameof(Departure), "Выезд");
 
-            view.AddIndent();
-            view.AddTextBox(nameof(Delta));
-            view.AddTextBox(nameof(ZSafety));
-        }
+        view.AddIndent();
+        view.AddTextBox(nameof(Delta));
+        view.AddTextBox(nameof(ZSafety));
+    }
 
-        public double Offset => ToolThickness / 2 + Delta;
+    public double Offset => ToolThickness / 2 + Delta;
 
-        public ProcessingWireSaw()
-        {
-            Caption = "Обработка Тросовый станок";
-            Machine = CAM.Machine.WireSawMachine;
-            Tool = new Tool { Type = ToolType.WireSaw };
-        }
+    public ProcessingWireSaw()
+    {
+        Caption = "Обработка Тросовый станок";
+        Machine = CAM.Machine.WireSawMachine;
+        Tool = new Tool { Type = ToolType.WireSaw };
     }
 }
