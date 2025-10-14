909dcf7d5ed6287451baa6c17604b75111fe7cb2
Миков Андрей <mikov@lanit.ru>
2025-10-14 11:15:21 +0500

    FindExtremum

--- Acad/Graph.cs
+++ Acad/Graph.cs
@@ -394,5 +394,30 @@ namespace CAM
         {
             return points.Aggregate(Point3d.Origin, (sum, point) => sum.Add(point.GetAsVector())) / points.Count();
         }
+
+        public static (double, int) FindExtremum(double[] x, double[] y, double x1, double x2, Func<double, double, double> extremumFunc, int startIndex = 0)
+        {
+            var i = startIndex;
+            while (x[i] < x1)
+                i++;
+
+            var index = i - 1;
+            var extremum = i == 0
+                ? y[0]
+                : LinearInterpolate(x1, x[i - 1], x[i], y[i - 1], y[i]);
+
+            while (i < x.Length && x[i] < x2)
+            {
+                extremum = extremumFunc(y[i], extremum);
+                i++;
+            }
+
+            if (i < x.Length)
+                extremum = LinearInterpolate(x2, x[i - 1], x[i], y[i - 1], y[i]);
+
+            return (extremum, index);
+
+            double LinearInterpolate(double x, double x0, double x1, double y0, double y1) => y0 + (y1 - y0) * (x - x0) / (x1 - x0);
+        }
     }
 }
