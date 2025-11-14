17d2642d865bd71491e4cf24cc27ad18d6040933
Andrey Mikov <airmay@gmail.com>
2025-11-14 20:35:21 +0500

    Update Helpers.cs

--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using Autodesk.AutoCAD.Geometry;
+using System;
 using System.Runtime.Serialization;
 
 namespace CAM.Utils;
@@ -10,29 +11,26 @@ public sealed class MyBinder : SerializationBinder
 
 public static class Helpers
 {
-    public static (double, int) FindMax(double[] x, double[] y, double x1, double x2, int startIndex = 0)
+    public static (double, int) FindMax(Point2d[] points, double x1, double x2, int startIndex = 0)
     {
         var i = startIndex;
-        while (x[i] < x1)
+        while (points[i].X < x1)
             i++;
 
         var (max, index) = i == 0
-            ? (y[0], 0)
-            : (GetY(x1), i - 1);
+            ? (points[0].Y, 0)
+            : (LinearInterpolate(x1, points[i - 1], points[i]), i - 1);
 
-        while (i < x.Length && x[i] <= x2)
-        {
-            max = Math.Max(y[i], max);
-            i++;
-        }
+        while (i < points.Length && points[i].X <= x2)
+            max = Math.Max(points[i++].Y, max);
 
-        if (i < x.Length)
-            max = Math.Max(GetY(x2), max);
+        if (i < points.Length)
+            max = Math.Max(LinearInterpolate(x2, points[i - 1], points[i]), max);
 
         return (max, index);
-
-        double GetY(double p) => LinearInterpolate(p, x[i - 1], x[i], y[i - 1], y[i]);
     }
+
+    public static double LinearInterpolate(double x, Point2d point0, Point2d point1) => point0.Y + (point1.Y - point0.Y) * (x - point0.X) / (point1.X - point0.X);
 
     public static double LinearInterpolate(double x, double x0, double x1, double y0, double y1) => y0 + (y1 - y0) * (x - x0) / (x1 - x0);
 }
